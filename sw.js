efa02439a1fec92e748b4a3985fc1eea95f8c0f6
awacamaradiallo708-hue <awacamaradiallo708@gmail.com>
2026-04-11 18:24:21 +0000

    mise a jour

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'chantier-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  console.log('Service Worker activé');
+});
